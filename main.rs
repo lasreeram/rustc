a1c9d4388b62a34251028e27b976dc8535d74217
vrindaparam <vrindaangel@gmail.com>
2025-01-19 15:25:54 -0500

    initial commit

---
+++ main.rs
@@ -0,0 +1,7 @@
+// Create a function in rust that calculates the average price of the items in the list. the list is passed as a parameter to the function.
+
+
+fn main() {
+    let avg = average_price();
+    println!("Average price: {}", avg);
+}
